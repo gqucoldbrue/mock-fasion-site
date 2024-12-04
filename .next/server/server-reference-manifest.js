b39464211f8b1cb9efa3ec4f8944f429f635db0e
gqucoldbrue <gqucoldbrue@gmail.com>
2024-12-03 23:17:56 -0500

    Further updates on upgrading dependencies

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"B/qD1XA8exnnehJQBFRpKae6EbQvo4P9sUVFGvO7VxA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
